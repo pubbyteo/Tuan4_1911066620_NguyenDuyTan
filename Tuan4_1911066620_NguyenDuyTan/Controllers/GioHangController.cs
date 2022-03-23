4bc6eb215b1c81306dee7723c32eaf8c80b89edc
duytannguyen183@gmail.com <duytannguyen183@gmail.com>
2022-03-23 11:07:51 +0700

    Nộp Bài ngày 23/3

--- Tuan4_1911066620_NguyenDuyTan/Controllers/GioHangController.cs
+++ Tuan4_1911066620_NguyenDuyTan/Controllers/GioHangController.cs
@@ -74,7 +74,54 @@ namespace Tuan4_1911066620_NguyenDuyTan.Controllers
             ViewBag.Tongsoluongsanpham = TongSoluongSanPham();
             return View(lstGiohang);
         }
-        public ActionResult GioHangPartial()
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            if (Session["TaiKhoan"] == null || Session["TaiKhoan"].ToString() == "*")
+            {
+                return RedirectToAction("DangNhap", "NguoiDung");
+            }
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "Sach");
+            }
+            List<Giohang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoluong();
+            ViewBag.Tongtien = TongTien();
+            ViewBag.Tongsoluongsanpham = TongSoluongSanPham();
+            return View(lstGiohang);
+        }
+        public ActionResult DatHang(FormCollection collection)
+        {
+            DonHang dh = new DonHang();
+            KhachHang kh = (KhachHang)Session["TaiKhoan"];
+            Sach s = new Sach();
+            List<Giohang> gh = Laygiohang();
+            var ngaygiao = String.Format("{0:MM/dd/yyyy}", collection["NgayGiao"]);
+            dh.makh = kh.makh;
+            dh.ngaydat = DateTime.Now;
+            dh.ngaygiao = DateTime.Parse(ngaygiao);
+            dh.giaohang = false;
+            dh.thanhtoan = false;
+            data.DonHangs.InsertOnSubmit(dh);
+            data.SubmitChanges();
+            foreach (var item in gh)
+            {
+                ChiTietDonHang ctdh = new ChiTietDonHang();
+                ctdh.madon = dh.madon;
+                ctdh.masach = item.masach;
+                ctdh.soluong = item.iSoluong;
+                ctdh.gia = (decimal)item.giaban;
+                s = data.Saches.Single(n => n.masach == item.masach);
+                s.soluongton -= ctdh.soluong;
+                data.SubmitChanges();
+                data.ChiTietDonHangs.InsertOnSubmit(ctdh);
+            }
+            data.SubmitChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("XacnhanDonhang", "GioHang");
+        }   
+         public ActionResult GioHangPartial()
         {
             ViewBag.Tongsoluong = TongSoluong();
             ViewBag.Tongtien = TongTien();
@@ -107,6 +154,14 @@ namespace Tuan4_1911066620_NguyenDuyTan.Controllers
             List<Giohang> lstGiohang = Laygiohang();
             lstGiohang.Clear();
             return RedirectToAction("GioHang");
+        }
+        public ActionResult Payment()
+        {
+          return View();
+        }
+        public ActionResult XacnhanDonHang()
+        {
+            return View();
         }
      }
 }
